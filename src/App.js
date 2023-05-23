f7699ad6bc337537e69ea485342264ae523c4102
Jessica Meeker <113938036+jesmeeker@users.noreply.github.com>
2023-05-23 10:37:36 -0500

    changed users fetch from local API to Caspio API

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ import Times from './components/calendar/Times';
 import DailyCalendar from './components/calendar/DailyCalendar';
 import { MultiSelect } from '@progress/kendo-react-dropdowns';
 import New from './components/calendar/New';
+import axios from 'axios';
 
 function App() {
 
@@ -36,67 +37,96 @@ function App() {
 
 
 
-
-
-  useEffect(() => {
-    fetch("http://localhost:8000/users", {
-      headers: {
-        Authorization: `Token 0be249c88238743e5b4a7ac370b5145730c28e20`,
-      },
-    }).then((res) => res.json())
-      .then(data => setUsers(data))
-  }, [])
-
-  const handleSubmit = () => {
-
-  }
-
-
-  const timeDisplay = () => {
-    if (user !== 0 && apptLength !== 0) {
-      return <>
-        <h3>Choose a time:</h3><New date={date} user={user} apptLength={apptLength} />
-        {/* <Times showTime={showTime} date={date} user={user} apptLength={apptLength} /> */}
-      </>
-    } else {
-      return <></>
+  const getAccessToken = async () => {
+    let urly = 'https://c1hby202.caspio.com/oauth/token';
+    try {
+      const resp = await axios.post(urly, 'grant_type=client_credentials&client_id=4cb05b2155f045d55244b1ca62a81b4def37241179198cc156&client_secret=3a0fde63c55e4802b47839f3217fa2939fa3a4e719afffb031');
+      let myAccessToken = resp.data.access_token;
+      getUsersData(myAccessToken)
+    } catch (err) {
+      console.error(err);
     }
+  };
+
+  const getUsersData = (myAccessToken) => {
+    let url2 = 'https://c1hby202.caspio.com/rest/v2/tables/CL_Users/records';
+    try {
+      const resp = axios.get(url2, {
+        headers: {
+          'Authorization': "Bearer " + myAccessToken
+        }
+      })
+        .then(response => {
+          setUsers(response.data.Result)
+        })
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+
+// useEffect(() => {
+//   fetch("http://localhost:8000/users", {
+//     headers: {
+//       Authorization: `Token 0be249c88238743e5b4a7ac370b5145730c28e20`,
+//     },
+//   }).then((res) => res.json())
+//     .then(data => setUsers(data))
+// }, [])
+
+const handleSubmit = () => {
+
+}
+
+
+const timeDisplay = () => {
+  if (user !== 0 && apptLength !== 0) {
+    return <>
+      <h3>Choose a time:</h3><New date={date} user={user} apptLength={apptLength} />
+      {/* <Times showTime={showTime} date={date} user={user} apptLength={apptLength} /> */}
+    </>
+  } else {
+    return <></>
   }
+}
+useEffect(() => {
+  getAccessToken()
+}, [])
 
-  useEffect(() => {
-    timeDisplay()
-  }, [date, user, apptLength])
+useEffect(() => {
+  timeDisplay()
+}, [date, user, apptLength])
 
-  // var getHdnFld = window.parent.document.getElementById('select[name="InsertRecordRoom"]').value;
+// var getHdnFld = window.parent.document.getElementById('select[name="InsertRecordRoom"]').value;
 
-  return (
-    <div className='app'>
-      <h1 className='header'>Book an Appointment</h1>
-      {/* <h1>{sRoom}</h1> */}
-      <h3 className=''>Choose your provider: <div class="select">
-        <select
-          name="user"
-          className=""
-          onChange={(event) => {
-            setUser(event.target.value)
-          }}>
-          <option key="user--0"
-            value="0">Select Provider</option>
-          {users.map(user => (
-            <option
-              key={`user--${user.id}`}
-              value={user.first_name}>
-              {user.first_name}
-            </option>
-          ))}
-        </select>
-      </div></h3>
-      <h3>Choose a date:</h3>
-      <div>
-        <Calendar onChange={setDate} value={date} onClickDay={() => setShowTime(true)} />
-      </div>
+return (
+  <div className='app'>
+    <h1 className='header'>Book an Appointment</h1>
+    {/* <h1>{sRoom}</h1> */}
+    <h3 className=''>Choose your provider: <div class="select">
+      <select
+        name="user"
+        className=""
+        onChange={(event) => {
+          setUser(event.target.value)
+        }}>
+        <option key="user--0"
+          value="0">Select Provider</option>
+        {users.map(user => (
+          <option
+            key={`user--${user.id}`}
+            value={user.First_Name}>
+            {user.First_Name}
+          </option>
+        ))}
+      </select>
+    </div></h3>
+    <h3>Choose a date:</h3>
+    <div>
+      <Calendar onChange={setDate} value={date} onClickDay={() => setShowTime(true)} />
+    </div>
 
-      {/* {date.length > 0 ? (
+    {/* {date.length > 0 ? (
         <p>
           <span>Start:</span>
           {date[0].toDateString()}
@@ -110,32 +140,32 @@ function App() {
         </p>
       )
       } */}
-      <h3>Choose an appointment length:</h3>
-      <select
-        name="user"
-        className=""
-        onChange={(event) => {
-          setLength(parseInt(event.target.value))
-        }}>
-        <option key="length--0"
-          value="0">Select Length</option>
-        {lengths.map(length => (
-          <option
-            key={`category--${length.id}`}
-            value={length.id}>
-            {length.label}
-          </option>
-        ))}
-      </select>
-      <div>
-        {
-          timeDisplay()
-        }
-      </div>
-      {/* <DailyCalendar /> */}
-
+    <h3>Choose an appointment length:</h3>
+    <select
+      name="user"
+      className=""
+      onChange={(event) => {
+        setLength(parseInt(event.target.value))
+      }}>
+      <option key="length--0"
+        value="0">Select Length</option>
+      {lengths.map(length => (
+        <option
+          key={`category--${length.id}`}
+          value={length.id}>
+          {length.label}
+        </option>
+      ))}
+    </select>
+    <div>
+      {
+        timeDisplay()
+      }
     </div>
-  )
+    {/* <DailyCalendar /> */}
+
+  </div>
+)
 }
 
 export default App;
